386586bf72858bb608afa86c177531bb8a29f603
Jeff Lent <jefflent01@gmail.com>
2018-11-11 19:35:39 -0700

    Initial commit

---
+++ src/main/java/com/galvanize/gmdb/gmdb/GmdbApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.gmdb.gmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbApplication.class, args);
+	}
+}
