bd3ea9f32737d1f915b22b5b457ef96f45ebde0d
chandra <chandra.polavarapu516@topper.wku.edu>
2018-11-14 15:41:35 -0700

    post for review still not working

---
+++ src/main/java/com/galvanize/gmdb/gmdb/entity/Review.java
@@ -0,0 +1,92 @@
+package com.galvanize.gmdb.gmdb.entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity(name="REVIEW")
+public class Review {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="REVIEW_ID")
+    private long reviewId;
+
+    @Column(name="MOVIE_ID", insertable = false, updatable = false, nullable = false)
+    private long movieId;
+
+    @Column(name="REVIEWER_ID", insertable = false, updatable = false, nullable = false)
+    private long reviewerId;
+
+    @Column(name="REVIEW_TEXT")
+    private String reviewText;
+
+    @Column(columnDefinition = "date", name="LAST_UPDATED")
+    private Date lastUpdate;
+
+    @ManyToOne
+    @JoinColumn(name = "MOVIE_ID", referencedColumnName = "MOVIE_ID")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name="REVIEWER_ID", referencedColumnName = "REVIEWER_ID")
+    private Reviewer reviewer;
+
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Reviewer getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(Reviewer reviewer) {
+        this.reviewer = reviewer;
+    }
+
+
+    public long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public long getReviewerId() {
+        return reviewerId;
+    }
+
+    public void setReviewerId(long reviewerId) {
+        this.reviewerId = reviewerId;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(long movieId) {
+        this.movieId = movieId;
+    }
+}
